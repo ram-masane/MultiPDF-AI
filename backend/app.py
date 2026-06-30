13dc734473a40629a2565c54ee9cab0afda05074
Ram Masane <ramsmasane5935@gmail.com>
2026-07-01 00:13:47 +0530

    Increase axios timeout

--- backend/app.py
+++ backend/app.py
@@ -24,6 +24,11 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+
+        "https://multi-pdf-ai.vercel.app",
+
+        # If Vercel creates preview deployments
+        "https://multi-pdf-sojjbn64q-ram-masane.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
