8ac79bb9ee8c7d7970a851c39abf7651065ca7ee
Ram Masane <ramsmasane5935@gmail.com>
2026-07-01 01:29:04 +0530

    Pin package versions

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
+from typing import List
 
 import os
 import shutil
+import asyncio
 
 from vector_store import build_vector_store
 from streaming import stream_answer
@@ -21,12 +23,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://multi-pdf-ai.vercel.app",
+    ],
     allow_origin_regex=r"https://.*\.vercel\.app",
-
-allow_origins=[
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -63,37 +65,38 @@ def home():
 # ======================================================
 
 @app.post("/upload")
-async def upload_pdfs(files: list[UploadFile] = File(...)):
-
+async def upload_pdfs(
+    files: List[UploadFile] = File(...)
+):
     print("=" * 80)
     print("Uploading PDFs...")
     print("=" * 80)
 
-    # Remove old PDFs
+    # Remove previous PDFs
     for filename in os.listdir(UPLOAD_FOLDER):
         path = os.path.join(UPLOAD_FOLDER, filename)
-
         if os.path.isfile(path):
             os.remove(path)
 
     uploaded_files = []
 
-    # Save new PDFs
     for file in files:
-
         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
 
         with open(file_path, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
 
-        # Return original filename
         uploaded_files.append(file.filename)
 
+    print("=" * 80)
     print("Building FAISS Index...")
+    print("=" * 80)
 
     build_vector_store()
 
+    print("=" * 80)
     print("Upload Complete!")
+    print("=" * 80)
 
     return {
         "success": True,
@@ -107,28 +110,19 @@ async def upload_pdfs(files: list[UploadFile] = File(...)):
 
 @app.post("/chat")
 def chat(request: ChatRequest):
-
     from rag import ask_pdf
-
     return ask_pdf(request.question)
 
 # ======================================================
 # Streaming Chat
 # ======================================================
 
-from fastapi.responses import StreamingResponse
-import asyncio
-
-
 @app.post("/chat/stream")
 async def chat_stream(request: ChatRequest):
 
     async def generate():
-
         for chunk in stream_answer(request.question):
-
             if hasattr(chunk, "content") and chunk.content:
-
                 for ch in chunk.content:
                     yield ch
                     await asyncio.sleep(0.01)
