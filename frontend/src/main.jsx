921d8295b41211e3c63cccc2021c2d280b04ace5
Ram Masane <ramsmasane5935@gmail.com>
2026-07-02 02:22:38 +0530

    fix: remove unused chat provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import ChatProvider from "./context/ChatProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
